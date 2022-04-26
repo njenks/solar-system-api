d6ef5527fb685c23168245abcda29069ff6a4b82
Gabriela Webb <gabw813@gmail.com>
2022-04-26 12:03:08 -0700

    added helper function

--- routes/planets.py
+++ routes/planets.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, jsonify
+import pdb
 
 class Planet(): 
     def __init__(self, id, name, description, diameter_km):
@@ -18,10 +19,8 @@ planets = [
     Planet(8, "Neptune", "Densest giant planet", 49244)
     ]
 
-planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
-
-@planets_bp.route("", methods=["GET"])
-def get_all_planets(): 
+def create_planet_dictionary_list(planets):
+    # turn into instance method? create second jsonified version
     response = []
     for planet in planets:
         response.append(
@@ -32,23 +31,29 @@ def get_all_planets():
                 "diameter_km": planet.diameter_km
             }
         )
-    return jsonify(response)
+    # pdb.set_trace()
+    return response
+
+planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
+
+@planets_bp.route("", methods=["GET"])
+def get_all_planets(): 
+    return jsonify(create_planet_dictionary_list(planets))
 
 @planets_bp.route("/<planet_id>", methods=["GET"])
 def get_one_planet(planet_id):
+    # create helper function to validate planets
     try:
         planet_id = int(planet_id)
     except ValueError:
         return jsonify({"Message": f"Planet ID {planet_id} is invalid. ID must be integer"}), 400
+
     chosen_planet = None
-    for planet in planets:
-        if planet.id == planet_id:
-            chosen_planet = {
-                "id": planet.id, 
-                "name": planet.name,
-                "description": planet.description, 
-                "diameter_km": planet.diameter_km
-            }
+    planet_list = create_planet_dictionary_list(planets)
+    for planet in planet_list:
+        if planet["id"] == planet_id:
+            chosen_planet = planet
+            return jsonify(chosen_planet)
+
     if chosen_planet is None:
         return jsonify({"Message":f"Planet {planet_id} not found."}), 404
-    return jsonify(chosen_planet)
